16599709066f63efa4715a052834f5b2e129a16c
Rociocora <161638863+Rociocora@users.noreply.github.com>
2024-07-04 12:46:46 +0200

    zara.com

--- src/features/login.feature
+++ src/features/login.feature
@@ -4,10 +4,10 @@ Feature: Login Zara
     Given the user is on zara login page
 
   Scenario: login with invalid username and password
-     When the user fill username with "rociocora7@gmail.com"
+     When the user fill email with "rociocora7@gmail.com"
      And the user fill password with "todoslosmeses7"
      And the user press login button
-     Then the user should see error message 
+     Then the user should see error message "La combinación de usuario y contraseña no se corresponden con ninguna cuenta en Zara.com."
 
 
   
--- src/steps/login_steps.ts
+++ src/steps/login_steps.ts
@@ -6,14 +6,21 @@ Given('the user is on zara login page', async function () {
   
 })
 
-When('I test playwrith record', async function (username: string) {
-    await this.page.goto('https://www.zara.com/');
-    await this.page.getByLabel('Email').click();
-    await this.page.getByLabel('Email').fill('rociocora7@gmail.com');
-    await this.page.getByLabel('Contraseña', { exact: true }).click();
-    await this.page.getByLabel('Contraseña', { exact: true }).fill('todoslosmeses7');
-    await this.page.getByRole('button', { name: 'INICIAR SESIÓN' }).click();
-    await this.page.getByRole('button', { name: 'Cerrar' }).click();
-
+When('the user fill email with {string}', async function (Email: string) {
+    await this.page.getByLabel('Email').fill(Email);
   })
-  
+
+When('the user fill password with {string}', async function (password: string) {
+    await this.page.getByLabel('Contraseña', { exact: true }).fill(password);
+})
+
+When('the user press login button', async function () {
+    await this.page.getByRole('button', { name: 'INICIAR SESIÓN' }).click();
+
+})
+
+
+Then('the user should see error message {string}', async function (errorMessage: string) {
+    await expect(this.page.locator('h1')).toContainText(errorMessage);
+})
+
